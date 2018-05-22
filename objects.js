a4c73e9199fee764791cb1836f0fe1ef6fd415c5
Andrea Witt <andreawitt14@hotmail.com>
2018-05-22 17:50:45 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,6 +7,7 @@ function updatePlaylist(playlist, artist, song){
 }
 
 function removeFromPlaylist(playlist, artist){
-  return delete playlist.artist
+  delete playlist.artist
+  return playlist
 }
 
